ead5ceed09be4b97e4fd8a306987eb32e8e062c4
akash <akashnaykodi2626@gmail.com>
2024-03-29 17:20:12 +0530

    day4Added

---
+++ FourthDay/QuotiennRemainder.java
@@ -0,0 +1,22 @@
+package Com.FourthDay;
+
+import java.util.Scanner;
+
+public class QuotiennRemainder {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please Enter Dividend : ");
+		int dividend=sc.nextInt();
+		System.out.println("Please Enter Divisor : ");
+		int divisor=sc.nextInt();
+		
+		int remainder=dividend%divisor;
+		int quotient=dividend/divisor;
+		
+		System.out.println("Remainder is : "+remainder+" and quotient is : "+quotient);
+
+	}
+
+}
